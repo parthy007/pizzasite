91ea0c81f12a4b7de66d71fe2c26ff13bed1fb49
parth adhikari <parthadhikari1812@gmail.com>
2024-03-02 20:30:36 +0530

    added padding

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export const metadata = {
   title: "Create Next App",
