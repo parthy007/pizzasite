63b81c4bc1ad2c990a6aa4462032edd7eefabc02
parth adhikari <parthadhikari1812@gmail.com>
2024-03-02 19:40:26 +0530

    initial setup

---
+++ src/components/layout/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section>
+      <h1>Everything is better with a Pizza!</h1>
+      <p>
+        Pizza is the missing piece that makes every day complete, a simple yet
+        delicious joy in life.
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
