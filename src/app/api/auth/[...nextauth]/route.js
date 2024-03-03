113e98b7a7b9595138de8a8f328ee51fdbdbfe34
parth adhikari <parthadhikari1812@gmail.com>
2024-03-03 22:16:38 +0530

    added api backend

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,34 @@
+import { User } from "@/models/User";
+import mongoose from "mongoose";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+const handler = NextAuth({
+  secret: process.env.SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      id: "credentials",
+      credentials: {
+        username: {
+          label: "Email",
+          type: "email",
+          placeholder: "test@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+        await mongoose.connect(process.env.MONGO_URL);
+        const user = User.findOne({ email });
+        const passwordOk = user && bcrypt.compareSync(password, user.password);
+
+        if (passwordOk) return user;
+        return null;
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
