b75f84309066c8c2d8186c40dcfcb8666fdc7cde
parth adhikari <parthadhikari1812@gmail.com>
2024-03-07 18:47:36 +0530

    added footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,26 @@
+import About from "@/components/layout/About";
 import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
 import HomeMenu from "@/components/layout/HomeMenu";
-import Image from "next/image";
+import SectionHeader from "@/components/layout/SectionHeader";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
       <HomeMenu />
+      <About />
+      <section className="text-center mt-16">
+        <SectionHeader subHeader={"Don't hesitate"} mainHeader={"Contact Us"} />
+        <div className="mt-6">
+          <a
+            href="tel:+91 8743523745"
+            className="text-gray-500 text-4xl underline font-bold"
+          >
+            +91 8743523745
+          </a>
+        </div>
+      </section>
     </>
   );
 }
