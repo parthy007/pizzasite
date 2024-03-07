043c0fbfaef51684909881151c72f5bc3087e19d
parth adhikari <parthadhikari1812@gmail.com>
2024-03-07 18:50:41 +0530

    removed required from password

--- src/models/User.js
+++ src/models/User.js
@@ -1,27 +1,15 @@
 import { Schema, model, models } from "mongoose";
-import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
+    name: { type: String },
     email: { type: String, required: true, unique: true },
     password: {
       type: String,
-      required: true,
-      validate: (pass) => {
-        if (!pass?.length || pass.length < 5) {
-          new Error("password must be at least 5 characters!");
-          return false;
-        }
-      },
     },
+    image: { type: String },
   },
   { timestamps: true }
 );
 
-userSchema.post("validate", (user) => {
-  const notHashedPassword = user.password;
-  const salt = bcrypt.genSaltSync(10);
-  user.password = bcrypt.hashSync(notHashedPassword, salt);
-});
-
-export const User = models?.User || model("users", userSchema);
+export const User = models?.User || model("User", userSchema);
