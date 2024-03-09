5c28a3b2736fa640bdbf373021086059ad2550eb
parth adhikari <parthadhikari1812@gmail.com>
2024-03-09 23:15:23 +0530

    added login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,77 +1,56 @@
 "use client";
-import Image from "next/image";
-import Link from "next/link";
-import React, { useState } from "react";
 import { signIn } from "next-auth/react";
+import Image from "next/image";
+import { useState } from "react";
 
-const LoginPage = () => {
+export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [creatingUser, setCreatingUser] = useState(false);
-  const [userCreated, setUserCreated] = useState(false);
-  const [error, setError] = useState(false);
+  const [loginInProgress, setLoginInProgress] = useState(false);
 
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-    setCreatingUser(true);
-    await signIn("credentials", { email, password });
-    setCreatingUser(false);
-  };
+  async function handleFormSubmit(ev) {
+    ev.preventDefault();
+    setLoginInProgress(true);
 
+    await signIn("credentials", { email, password, callbackUrl: "/" });
+
+    setLoginInProgress(false);
+  }
   return (
     <section className="mt-16">
-      <h1 className="text-primary font-bold text-4xl text-center mb-4">
-        Login
-      </h1>
-      {userCreated && (
-        <div className="text-center text-green-700">
-          User successfully created.&nbsp;
-          <Link href={"/login"} className="underline">
-            Login
-          </Link>{" "}
-          &raquo;
-        </div>
-      )}
-      {error && (
-        <div className="text-center text-red-600 underline">
-          Error has occured.&nbsp; Please try again
-        </div>
-      )}
-      <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+      <h1 className="text-center text-primary text-4xl mb-4">Login</h1>
+      <form className="max-w-xs mx-auto" onSubmit={handleFormSubmit}>
         <input
           type="email"
-          disabled={creatingUser}
-          placeholder="Email"
+          name="email"
+          placeholder="email"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          disabled={loginInProgress}
+          onChange={(ev) => setEmail(ev.target.value)}
         />
         <input
           type="password"
-          disabled={creatingUser}
-          placeholder="Password"
+          name="password"
+          placeholder="password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          disabled={loginInProgress}
+          onChange={(ev) => setPassword(ev.target.value)}
         />
-        <button type="submit" disabled={creatingUser}>
+        <button disabled={loginInProgress} type="submit">
           Login
         </button>
-        <div className="text-center text-gray-500 my-4">
+        <div className="my-4 text-center text-gray-500">
           or login with provider
         </div>
-        <button className="flex gap-4 justify-center ">
-          <Image src={"/google.png"} width={24} height={24} alt="googlelogo" />
-          Login with Google
+        <button
+          onClick={() => signIn("google", { callbackUrl: "/" })}
+          className="flex gap-4 justify-center"
+          type="button"
+        >
+          <Image src={"/google.png"} alt={""} width={24} height={24} />
+          Login with google
         </button>
-        <div className="text-center my-4 text-gray-500">
-          Create new account.{" "}
-          <Link className="underline" href={"/register"}>
-            Register
-          </Link>
-          &raquo;
-        </div>
       </form>
     </section>
   );
-};
-
-export default LoginPage;
+}
