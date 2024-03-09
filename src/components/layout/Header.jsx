f59aa543178bbaf3c380eac6b2c09d9118f1209d
parth adhikari <parthadhikari1812@gmail.com>
2024-03-09 23:18:44 +0530

    added updated session

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,23 +1,63 @@
+"use client";
+import { getSession, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 
 const Header = () => {
+  const session = useSession();
+  const status = session?.status;
+  const userData = session.data?.user;
+  let userName = userData?.name || userData?.email;
+  if (userName && userName.includes(" ")) {
+    userName = userName.split(" ")[0];
+  }
+  // console.log(session.status);
+
+  useEffect(() => {
+    if (!session) {
+      const fetchSessionData = async () => {
+        await getSession();
+      };
+      fetchSessionData();
+    }
+  }, []);
+
   return (
     <header className="flex items-center justify-between">
-      <Link href="" className="text-primary font-semibold text-2xl">
-        Pizza
-      </Link>
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-        <Link href={""}>Home</Link>
+        <Link href="/" className="text-primary font-semibold text-2xl">
+          Pizza
+        </Link>
+        <Link href={"/"}>Home</Link>
         <Link href={""}>Menu</Link>
         <Link href={""}>About</Link>
         <Link href={""}>Contact</Link>
-        <Link
-          href={""}
-          className="bg-primary rounded-full text-white px-8 py-2"
-        >
-          Login
-        </Link>
+      </nav>
+      <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+        {status === "authenticated" && (
+          <>
+            <Link href={"/profile"} className="whitespace-nowrap">
+              Hello, {userName}
+            </Link>
+            <button
+              className="bg-primary rounded-full text-white px-8 py-2"
+              onClick={() => signOut()}
+            >
+              Logout
+            </button>
+          </>
+        )}
+        {status === "unauthenticated" && (
+          <>
+            <Link href={"/login"}>Login</Link>
+            <Link
+              href={"/register"}
+              className="bg-primary rounded-full text-white px-8 py-2"
+            >
+              Register
+            </Link>
+          </>
+        )}
       </nav>
     </header>
   );
